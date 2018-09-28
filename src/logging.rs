27d3610fbe5f66fb1a6167f0a532916e8d402732
Yngve Hestem <yjhestem@stud.ntnu.no>
2018-09-28 19:30:53 +0200

    Adding new logging system (fern) to security gate

---
+++ src/logging.rs
@@ -0,0 +1,52 @@
+use std::io;
+
+pub fn setup_logging(verbosity: u64) -> Result<(), fern::InitError> {
+    let mut base_config = fern::Dispatch::new();
+
+    base_config = match verbosity {
+        0 => base_config.level(log::LevelFilter::Info),
+        1 => base_config.level(log::LevelFilter::Debug),
+        _2_or_more => base_config.level(log::LevelFilter::Trace),
+    };
+
+    // Separate file config so we can include year, month and day in file logs
+    let file_config = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .chain(fern::log_file("controller.log")?);
+
+    let stdout_config = fern::Dispatch::new()
+        .format(|out, message, record| {
+            // special format for debug messages coming from our own crate.
+            if record.level() > log::LevelFilter::Info && record.target() == "cmd_program" {
+                out.finish(format_args!(
+                    "---\nDEBUG: {}: {}\n---",
+                    chrono::Local::now().format("%H:%M:%S"),
+                    message
+                ))
+            } else {
+                out.finish(format_args!(
+                    "[{}][{}][{}] {}",
+                    chrono::Local::now().format("%H:%M"),
+                    record.target(),
+                    record.level(),
+                    message
+                ))
+            }
+        })
+        .chain(io::stdout());
+
+    base_config
+        .chain(file_config)
+        .chain(stdout_config)
+        .apply()?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -22,13 +22,14 @@ extern crate failure_derive;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
+extern crate clap;
 
 mod auth;
 mod content;
+pub mod logging;
 
 use rocket::fairing::AdHoc;
 use std::io::Write;
-use std::net::SocketAddr;
 use chrono::Local;
 use env_logger::Builder;
 use log::LevelFilter;
@@ -38,21 +39,38 @@ use log::LevelFilter;
 type JsonResult<T, E> = Result<rocket_contrib::Json<T>, rocket_contrib::Json<E>>;
 
 fn main() {
+    // Logging
+    let cmd_arguments = clap::App::new("cmd-program")
+        .arg(
+            clap::Arg::with_name("verbose")
+                .short("v")
+                .long("verbose")
+                .multiple(true)
+                .help("Increases logging verbosity each use for up to 3 times"),
+        )
+        .get_matches();
+
+    let verbosity: u64 = cmd_arguments.occurrences_of("verbose");
+
+    logging::setup_logging(verbosity).expect("failed to initialize logging.");
+
+    info!("Starting program");
+
     rocket::ignite()
-        .attach(AdHoc::on_request(|req, _| {
-            Builder::new()
-                .format(|buf, record| {
-                    writeln!(buf,
-                        "{} [{}] - IP {:?}: {}",
-                        Local::now().format("%Y-%m-%dT%H:%M:%S"),
-                        record.level(),
-                        req.remote(),                    // Returns an ip or None if nothing is found.
-                        record.args()
-                    )
-                })
-                .filter(None, LevelFilter::Info)
-                .init();
-        }))
+        // .attach(AdHoc::on_request(|req, _| {
+        //     Builder::new()
+        //         .format(|buf, record| {
+        //             writeln!(buf,
+        //                 "{} [{}] - IP {:?}: {}",
+        //                 Local::now().format("%Y-%m-%dT%H:%M:%S"),
+        //                 record.level(),
+        //                 req.remote(),                    // Returns an ip or None if nothing is found.
+        //                 record.args()
+        //             )
+        //         })
+        //         .filter(None, LevelFilter::Info)
+        //         .init();
+        // }))
         .mount("/", routes![content::routes::index])
         .mount("/", routes![content::routes::static_file])
         .mount("/api/", routes![auth::routes::auth])
@@ -65,4 +83,6 @@ fn main() {
         .mount("/api", routes![content::routes::get_user])
         .mount("/api", routes![content::routes::post_content])
         .launch();
+
+        info!("Set up routes.");
 }
