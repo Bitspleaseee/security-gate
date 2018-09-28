f67395b0fa1358c918d6ae6b25add8b154832afd
Yngve Hestem <yjhestem@stud.ntnu.no>
2018-09-28 11:43:15 +0200

    Change request guard to only check if it exist a token, the rest will be done in a function

--- src/auth/responses.rs
+++ src/auth/responses.rs
@@ -1,9 +1,9 @@
-// use rocket::Outcome;
-// use rocket::http::Status;
-// use rocket::http::Cookies;
-// use crate::auth::api::{authenticated, USER_TOKEN_NAME};
-// use rocket::request::{self, Request, FromRequest};
-// use rocket_contrib::Json;
+use rocket::Outcome;
+use rocket::http::Status;
+use rocket::http::Cookies;
+use crate::auth::api::{Token, USER_TOKEN_NAME};
+use rocket::request::{self, Request, FromRequest};
+use rocket_contrib::Json;
 
 #[derive(Serialize, Deserialize)]
 #[serde(
@@ -31,25 +31,27 @@ pub enum AuthError {
     InvalidPassword,
 }
 
-struct Autenticated(u32);
+impl<'a, 'r> FromRequest<'a, 'r> for Token<'a> {
+    type Error = AuthError;
 
-// impl<'a, 'r> FromRequest<'a, 'r> for Autenticated {
-//     type Error = AuthError;
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Token<'a>, AuthError> {
+        let cookie = request.cookies()
+        .get_private(USER_TOKEN_NAME);
 
-//     fn from_request(request: &'a Request<'r>) -> request::Outcome<Autenticated, AuthError> {
-//         let cookie = request.cookies()
-//         .get_private(USER_TOKEN_NAME);
-//         //TODO: Fix errormessage:
-//         //.ok_or(AuthError::MissingToken)
-//         //.map_err(Json)?;
 
-//         let result = authenticated(&cookie);
-
-//         if result.is_ok() {
-//             return Outcome::Success(Autenticated(result.id));
-//         } else {
-//             return Outcome::Failure((Status::BadRequest, AuthError::MissingToken));
-//         }
-//     }
-// }
-
+         match cookie {
+            Some(token) => {
+                // Get a comment
+                //let result = controller.get_comment(id);
+                info!("Getting request with token {:?}", token);
+                return Outcome::Success(Token(cookie));
+            }
+            None => {
+                // Get all comments
+                //let result = controller.get_all_comments();
+                trace!("Getting all threads");
+                return Outcome::Failure((Status::BadRequest, AuthError::MissingToken));
+            }
+        }
+    }
+}
