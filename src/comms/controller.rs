84deb5aec7b2db6d643266b124f66c87791e4585
Ole Martin Ruud <barskern@outlook.com>
2018-10-06 04:57:18 +0200

    Update 'service!' definition

--- src/comms/controller.rs
+++ src/comms/controller.rs
@@ -1,35 +1,33 @@
 use datatypes::content::requests::*;
 use datatypes::content::responses::*;
-use datatypes::error::ResponseError;
 
 pub const CONTROLLER_IP: &str = "localhost:1000";
 
 service! {
-    rpc get_user(payload: GetUserPayload) -> UserPayload | ResponseError;
-    rpc add_user(payload: AddUserPayload) -> UserPayload | ResponseError;
-    rpc edit_user(payload: EditUserPayload) -> UserPayload | ResponseError;
-    rpc upload_avatar(payload: UploadAvatarPayload) -> UserPayload | ResponseError;
+    rpc get_user(payload: GetUserPayload) -> UserPayload | ContentError;
+    rpc add_user(payload: AddUserPayload) -> UserPayload | ContentError;
+    rpc edit_user(payload: EditUserPayload) -> UserPayload | ContentError;
+    rpc upload_avatar(payload: UploadAvatarPayload) -> UserPayload | ContentError;
 
-    rpc get_category(payload: GetCategoryPayload) -> CategoryPayload | ResponseError;
-    rpc get_categories(payload: GetHiddenPayload) -> Vec<CategoryPayload> | ResponseError;
-    rpc add_category(payload: AddCategoryPayload) -> CategoryPayload | ResponseError;
-    rpc edit_category(payload: EditCategoryPayload) -> CategoryPayload | ResponseError;
-    rpc hide_category(payload: HideCategoryPayload) -> CategoryPayload | ResponseError;
+    rpc get_category(payload: GetCategoryPayload) -> CategoryPayload | ContentError;
+    rpc get_categories(payload: GetHiddenPayload) -> Vec<CategoryPayload> | ContentError;
+    rpc add_category(payload: AddCategoryPayload) -> CategoryPayload | ContentError;
+    rpc edit_category(payload: EditCategoryPayload) -> CategoryPayload | ContentError;
+    rpc hide_category(payload: HideCategoryPayload) -> CategoryPayload | ContentError;
 
-    rpc get_thread(payload: GetThreadPayload) -> ThreadPayload | ResponseError;
-    rpc get_threads(payload: GetThreadsPayload) -> Vec<ThreadPayload> | ResponseError;
-    rpc get_all_threads(payload: GetHiddenPayload) -> Vec<ThreadPayload> | ResponseError;
-    rpc add_thread(payload: AddThreadPayload) -> ThreadPayload | ResponseError;
-    rpc edit_thread(payload: EditThreadPayload) -> ThreadPayload | ResponseError;
-    rpc hide_thread(payload: HideThreadPayload) -> ThreadPayload | ResponseError;
+    rpc get_thread(payload: GetThreadPayload) -> ThreadPayload | ContentError;
+    rpc get_threads(payload: GetThreadsPayload) -> Vec<ThreadPayload> | ContentError;
+    rpc get_all_threads(payload: GetHiddenPayload) -> Vec<ThreadPayload> | ContentError;
+    rpc add_thread(payload: AddThreadPayload) -> ThreadPayload | ContentError;
+    rpc edit_thread(payload: EditThreadPayload) -> ThreadPayload | ContentError;
+    rpc hide_thread(payload: HideThreadPayload) -> ThreadPayload | ContentError;
 
-    rpc get_comment(payload: GetCommentPayload) -> CommentPayload | ResponseError;
-    rpc get_comments(payload: GetCommentsPayload) -> Vec<CommentPayload> | ResponseError;
-    rpc get_all_comments(payload: GetHiddenPayload) -> Vec<CommentPayload> | ResponseError;
-    rpc add_comment(payload: AddCommentPayload) -> CommentPayload | ResponseError;
-    rpc edit_comment(payload: EditCommentPayload) -> CommentPayload | ResponseError;
-    rpc hide_comment(payload: HideCommentPayload) -> CommentPayload | ResponseError;
+    rpc get_comment(payload: GetCommentPayload) -> CommentPayload | ContentError;
+    rpc get_comments(payload: GetCommentsPayload) -> Vec<CommentPayload> | ContentError;
+    rpc get_all_comments(payload: GetHiddenPayload) -> Vec<CommentPayload> | ContentError;
+    rpc add_comment(payload: AddCommentPayload) -> CommentPayload | ContentError;
+    rpc edit_comment(payload: EditCommentPayload) -> CommentPayload | ContentError;
+    rpc hide_comment(payload: HideCommentPayload) -> CommentPayload | ContentError;
 
-    rpc search(payload: SearchPayload) -> SearchResultsPayload | ResponseError;
-
+    rpc search(payload: SearchPayload) -> SearchResultsPayload | ContentError;
 }
