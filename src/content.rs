36446aaf97f77ace33d127d9946e3d1277fbd458
Yngve Hestem <yjhestem@stud.ntnu.no>
2018-10-08 12:14:22 +0200

    Fixing some error

--- src/content.rs
+++ src/content.rs
@@ -28,7 +28,7 @@ fn connect_to_controller() -> Result<ControllerClient, ResponseError> {
 
 fn is_admin_or_mod(token: Token) -> bool {
     let role = connect_to_auth()
-        .map_err(Json)?
+        .map_err(Json)
         .get_user_role(TokenPayload::new (None,token))
         .map_err(Json);
     let role = role.unwrap_or(Role::User);
